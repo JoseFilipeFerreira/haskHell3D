db5646f3111e6a73e7d0bebf3be5a4203709ffc4
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-06-13 23:43:02 +0100

    version using Data.Geometry.Polygon

--- src/Desenha_Estado.hs
+++ src/Desenha_Estado.hs
@@ -4,38 +4,67 @@ Description : Module draw state haskHell 3D
 -}
 module Desenha_Estado where
 
-    import Data_structures
-    import Constantes
-    import Reage_Tempo
-    import Graphics.Gloss.Data.Picture          -- importar o tipo Picture
-    import Graphics.Gloss.Data.Color
-    import Data.List
-    import Data.Ord
+import           Data_structures
+import           Constantes
+import           Reage_Tempo
+import           Graphics.Gloss.Data.Picture          -- importar o tipo Picture
+import           Graphics.Gloss.Data.Color
+import           Data.List
+import           Data.Ord
+import           Data.Geometry.Polygon
+import           Data.Geometry.Point
+import qualified Data.CircularSeq as C
+import           Data.Ext
 
-    -- * Desenhar um 'Estado'
-    -- | Função responsável por desenhar o jogo.
-    desenhaEstado :: Estado -> Picture
-    desenhaEstado e = Rotate (-90) $ Scale 20 20 $ Pictures[drawnWalls, drawnPlayer e, drawViewBox]
-        where
-            drawnWalls = Pictures $ map drawWall (mapa e)
+-- * Desenhar um 'Estado'
+-- | Função responsável por desenhar o jogo.
+desenhaEstado :: Estado -> Picture
+desenhaEstado e =  Scale 20 20 $ Pictures[drawnWalls, drawnPlayer e, Line viewBox]
+    where
+        drawnWalls = Pictures $ map drawWall $ filterOutsideWalls $ mapa e
     
-    drawnPlayer :: Estado -> Picture
-    drawnPlayer e = Rotate (90) $ color red $ Polygon[(0.5,-0.5),(-0.5,-0.5),(0,0.5)]
+drawnPlayer :: Estado -> Picture
+drawnPlayer e = Rotate (90) $ color red $ Polygon[(0.5,-0.5),(-0.5,-0.5),(0,0.5)]
 
-    drawWall :: Wall -> Picture
-    drawWall (Wall p1 p2 col) = color col $ Line[p1, p2]
+drawWall :: Wall -> Picture
+drawWall (Wall p1 p2 col) = color col $ Line[p1, p2]
 
-    drawViewBox :: Picture
-    drawViewBox = Line[pn1, pn2, pf2, pf1]
-        where
-            pn1 = (nearPlane, nearPlane * tan $ grauToRad -viewAngle/2)
-            pn2 = (nearPlane, nearPlane * tan $ grauToRad viewAngle/2)
-            pf1 = (farPlane , farPlane  * tan $ grauToRad $ -viewAngle/2)
-            pf2 = (farPlane , farPlane  * tan $ grauToRad $  viewAngle/2) 
 
-    -- | Draws the aim in the screen
-    target:: Color -> Float -> Picture
-    target col r = color col $ Pictures[pol, Rotate 90 pol, Rotate 180 pol, Rotate (-90) pol]
-        where
-            u = r/14
-            pol = Translate (u/2) (u/2) $ Polygon [(0,0), (0,u*6), (u, u*6), (u, u), (u*6,u), (u*6, 0)]
+filterOutsideWalls:: [Wall] -> [Wall]
+filterOutsideWalls = filter (isInsideWall) 
+    where
+        isInsideWall:: Wall -> Bool
+        isInsideWall (Wall (x1, y1) (x2, y2) _) = insidePolygon (point2 x1 y1) viewBoxPoly
+                                               || insidePolygon (point2 x2 y2) viewBoxPoly
+
+viewBoxPoly :: SimplePolygon () Float
+viewBoxPoly = SimplePolygon . C.fromList . map ext $ [ point2 pn1x pn1y
+                                                     , point2 pn2x pn2y
+                                                     , point2 pf2x pf2y
+                                                     , point2 pf1x pf1y
+                                                     , point2 pn1x pn1y
+                                                     ]
+    where
+        pn1x = nearPlane
+        pn1y = nearPlane * tan (grauToRad $ -viewAngle/2)
+        pn2x = nearPlane
+        pn2y = nearPlane * tan (grauToRad $  viewAngle/2)
+        pf1x = farPlane
+        pf1y = farPlane  * tan (grauToRad $ -viewAngle/2)
+        pf2x = farPlane 
+        pf2y = farPlane  * tan (grauToRad $  viewAngle/2) 
+
+viewBox :: [(Coor)]
+viewBox = [pn1, pn2, pf2, pf1, pn1]
+    where
+        pn1 = (nearPlane, nearPlane * tan (grauToRad $ -viewAngle/2))
+        pn2 = (nearPlane, nearPlane * tan (grauToRad $  viewAngle/2))
+        pf1 = (farPlane , farPlane  * tan (grauToRad $ -viewAngle/2))
+        pf2 = (farPlane , farPlane  * tan (grauToRad $  viewAngle/2)) 
+
+-- | Draws the aim in the screen
+target:: Color -> Float -> Picture
+target col r = color col $ Pictures[pol, Rotate 90 pol, Rotate 180 pol, Rotate (-90) pol]
+    where
+        u = r/14
+        pol = Translate (u/2) (u/2) $ Polygon [(0,0), (0,u*6), (u, u*6), (u, u), (u*6,u), (u*6, 0)]
--- src/main.hs
+++ src/main.hs
@@ -5,70 +5,70 @@ Description : Módulo da Tarefa 5 para LI1 17/18
 Módulo para a realização da Tarefa 5 de LI1 em 2017/18.
 -}
 module Main where
-    import Graphics.Gloss
-    import Graphics.Gloss.Data.Picture          -- importar o tipo Picture
-    import Graphics.Gloss.Interface.Pure.Game
-    import Data.Ord
-    import Data_structures
-    import Reage_Tempo
-    import Reage_Evento
-    import Desenha_Estado
+import Graphics.Gloss
+import Graphics.Gloss.Data.Picture          -- importar o tipo Picture
+import Graphics.Gloss.Interface.Pure.Game
+import Data.Ord
+import Data_structures
+import Reage_Tempo
+import Reage_Evento
+import Desenha_Estado
 
-    {-|
-    Função principal usada para animar um jogo completo.
-    Compilar com o GHC.
-    -}
-    main :: IO ()
-    main = do inicio <- initialState
-              joga inicio
-    
-    -- | Função que controla o jogo
-    joga:: Estado -> IO()
-    joga inicio = play
-          (InWindow  "haskHell 3D" (1280,720) (0,0) )       -- Janela onde irá correr o jogo      
-          (greyN 0.8)                                       -- Cor do fundo da janela.
-          30                                                -- Frame Rate
-          inicio                                            -- Fundo inicial do jogo.
-          desenhaEstado                                     -- Desenha o Estado do jogo.
-          reageEvento                                       -- Reage a um Evento.
-          reageTempo                                        -- Reage ao passar do Tempo.
+{-|
+Função principal usada para animar um jogo completo.
+Compilar com o GHC.
+-}
+main :: IO ()
+main = do inicio <- initialState
+          joga inicio
 
-    initialState :: IO Estado
-    initialState = do  
-                        let defaultPlayer = Player 0
-                        let defaultAction = Actions False False False False False
-                        return Estado { mapa = testMap
-                                      , player = defaultPlayer
-                                      , actions = defaultAction
-                                      , winSize = (0,0)
-                                      }
-    
-    testMap::[Wall]
-    testMap = [ Wall (-1,-1) (-1,6)  black
-              , Wall (-1,-1) (10,-1) black
-              , Wall (10,-1) (10,1) black
-              , Wall (10,1) (15,1) black
-              , Wall (15,1) (15,3) black
-              , Wall (15,3) (16,3) black
-              , Wall (16,3) (16,5) black
-              , Wall (16,5) (17,5) black
-              , Wall (17,5) (17,12) black
-              , Wall (17,12) (17,13) black
-              , Wall (17,13) (13,13) black
-              , Wall (13,13) (13,14) black
-              , Wall (13,14) (1,14) black
-              , Wall (1,14) (1,8) black
-              , Wall (1,8) (4,8) black
-              , Wall (4,8) (4,6) black
-              , Wall (4,6) (-1,6) black
-              , Wall (6,6) (6,8) black
-              , Wall (6,8) (9,8) black
-              , Wall (9,8) (9,9) black
-              , Wall (9,9) (13,9) black
-              , Wall (13,9) (13,5) black
-              , Wall (13,5) (12,5) black
-              , Wall (12,5) (12,4) black
-              , Wall (12,4) (10,4) black
-              , Wall (10,4) (10,6) black
-              , Wall (10,6) (6,6) black
-              ]
+-- | Função que controla o jogo
+joga:: Estado -> IO()
+joga inicio = play
+        (InWindow  "haskHell 3D" (1280,720) (0,0) )       -- Janela onde irá correr o jogo      
+        (greyN 0.8)                                       -- Cor do fundo da janela.
+        30                                                -- Frame Rate
+        inicio                                            -- Fundo inicial do jogo.
+        desenhaEstado                                     -- Desenha o Estado do jogo.
+        reageEvento                                       -- Reage a um Evento.
+        reageTempo                                        -- Reage ao passar do Tempo.
+
+initialState :: IO Estado
+initialState = do  
+                let defaultPlayer = Player 0
+                let defaultAction = Actions False False False False False
+                return Estado { mapa = testMap
+                              , player = defaultPlayer
+                              , actions = defaultAction
+                              , winSize = (0,0)                                  
+                              }
+
+testMap::[Wall]
+testMap = [ Wall (-1,-1) (-1,6)  black
+          , Wall (-1,-1) (10,-1) black
+          , Wall (10,-1) (10,1) black
+          , Wall (10,1) (15,1) black
+          , Wall (15,1) (15,3) black
+          , Wall (15,3) (16,3) black
+          , Wall (16,3) (16,5) black
+          , Wall (16,5) (17,5) black
+          , Wall (17,5) (17,12) black
+          , Wall (17,12) (17,13) black
+          , Wall (17,13) (13,13) black
+          , Wall (13,13) (13,14) black
+          , Wall (13,14) (1,14) black
+          , Wall (1,14) (1,8) black
+          , Wall (1,8) (4,8) black
+          , Wall (4,8) (4,6) black
+          , Wall (4,6) (-1,6) black
+          , Wall (6,6) (6,8) black
+          , Wall (6,8) (9,8) black
+          , Wall (9,8) (9,9) black
+          , Wall (9,9) (13,9) black
+          , Wall (13,9) (13,5) black
+          , Wall (13,5) (12,5) black
+          , Wall (12,5) (12,4) black
+          , Wall (12,4) (10,4) black
+          , Wall (10,4) (10,6) black
+          , Wall (10,6) (6,6) black
+          ]
