65eabe1d9af75228a436e9bbbf52ebf9f8a5d610
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-06-19 22:11:53 +0100

    improve draw lines in 3D

--- src/Draw_State3D.hs
+++ src/Draw_State3D.hs
@@ -23,8 +23,6 @@ drawAll3D e = Pictures $ [floor, sky] ++ drawParts e m en ++ [iron]
         sky   = Color (makeColor (135/255) (206/255) (250/255) 1) $ Polygon [(-sx, 0), (sx,0), (sx,  sy), (-sx, sy)]
         iron  = target red $ min (fromIntegral(sxI)/22)(fromIntegral(syI)/22)
 
-
-
 drawParts:: Estado -> Mapa -> Enemies -> [Picture]
 drawParts e [] [] = []
 drawParts e w  [] = map (drawWall3D  e) w
@@ -43,24 +41,21 @@ drawEnemy3D:: Estado -> Enemy -> Picture
 drawEnemy3D e en = drawLine3D e enemyHeigth red (p1E en) (p2E en)
 
 drawLine3D:: Estado -> Float -> Color -> Coor -> Coor -> Picture
-drawLine3D e h col p1 p2 = Pictures[ Color col                 $ Polygon  allPoints
-                                 , Color (contrastColor col) $ lineLoop allPoints
-                                 ]
+drawLine3D e h col (x1, y1) (x2, y2) = Pictures[ Color col                 $ Polygon  allPoints
+                                               , Color (contrastColor col) $ lineLoop allPoints
+                                               ]
     where
         allPoints = [(xW1,pH1), (xW1,h1), (xW2,h2), (xW2,pH2)]
-        (sx, sy) = winSize e
-        d1  = distCoor (0,0) p1
-        d2  = distCoor (0,0) p2
+        d1  = distCoor (0,0) (x1, y1)
+        d2  = distCoor (0,0) (x2, y2)
         hW  = h - playerHeigth
-        pH1 = (playerHeigth/d1) * nearPlane * (realToFrac(fst $ winSize e) / ((snd $ head viewBox)*2)) 
-        pH2 = (playerHeigth/d2) * nearPlane *  (realToFrac(fst $ winSize e) / ((snd $ head viewBox)*2))
-        h1  = -(hW / d1)         * nearPlane * (realToFrac(fst $ winSize e) / ((snd $ head viewBox)*2))
-        h2  = -(hW / d2)         * nearPlane * (realToFrac(fst $ winSize e) / ((snd $ head viewBox)*2))
-        xW1 = xPostionPoint e p1
-        xW2 = xPostionPoint e p2
-
-        xPostionPoint :: Estado -> Coor -> Float
-        xPostionPoint e (x, y) = (realToFrac(fst $ winSize e) / ((snd $ head viewBox)*2)) * nearPlane * (y/x)
+        ratio = nearPlane * (realToFrac(fst $ winSize e) / ((snd $ head viewBox)*2))
+        pH1 =  (playerHeigth/d1) * ratio 
+        pH2 =  (playerHeigth/d2) * ratio
+        h1  = -(hW / d1)         * ratio
+        h2  = -(hW / d2)         * ratio
+        xW1 =  (y1 / x1)         * ratio
+        xW2 =  (y2 / x2)         * ratio
 
 -- | Draws the aim in the screen
 target:: Color -> Float -> Picture
